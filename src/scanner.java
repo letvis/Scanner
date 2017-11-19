0c2e2d4701e366252100eedc6ca67e248cf0631b
letvis <letvisales@ukr.net>
2017-11-19 08:48:22 +0200

    Сканер

---
+++ src/scanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class scanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Имя:          ");
+        String str = scanner.next();
+        System.out.print("Город:        ");
+        String str2 = scanner.next();
+        System.out.print("Возраст:      ");
+        int Возраст = scanner.nextInt();
+        System.out.print("Хобби:        ");
+        String str3 = scanner.next();
+
+        System.out.println("СПАСИБО ЗА ОБУЧЕНИЕ");
+
+    }
+}
