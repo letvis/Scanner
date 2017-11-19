e196db4db74e83ff19204897cf26e16564930c64
letvis <letvisales@ukr.net>
2017-11-19 13:27:07 +0200

    Scanner в строчку

---
+++ src/scanner2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class scanner2 {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = "Человек по имени  ";
+        String city = "живет в городе  ";
+        String age = "Этому человеку  ";
+        String hobby = "лет и любит он заниматься  ";
+        String str = scanner.next();
+        String str2 = scanner.next();
+        int Возраст = scanner.nextInt();
+        String str3 = scanner.next();
+        System.out.println(ANSI_BLUE+name+ANSI_RESET + str +" " + ANSI_RED+city+ANSI_RESET +str2);
+        System.out.println(ANSI_BLUE+age+ANSI_RESET+Возраст+ANSI_BLUE+" лет"+ANSI_RESET+ANSI_RED+hobby+ANSI_RESET+str3);
+
+    }
+}
